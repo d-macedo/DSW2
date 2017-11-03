a61bb2b84ebdbd2b2a8f51426008204eaf262be0
paguii <pagui-guilherme@hotmail.com>
2017-11-03 01:28:33 -0200

    Arrumando URl de acesso

--- src/main/java/br/edu/ifsp/ingresso/controllers/LoginController.java
+++ src/main/java/br/edu/ifsp/ingresso/controllers/LoginController.java
@@ -1,10 +1,12 @@
 package br.edu.ifsp.ingresso.controllers;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
 
 @Controller
 public class LoginController {
 	
+	@Path("/login")
 	public void login() {
 		
 	}
